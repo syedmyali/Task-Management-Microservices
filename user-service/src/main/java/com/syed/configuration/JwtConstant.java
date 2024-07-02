53fe79cd5a3bf4aaf4875cf4c37b878e9c6528f8
Syed Ali <syounusa@yahoo.com>
2024-07-02 17:38:34 +0100

    Secret key field changed

--- user-service/src/main/java/com/syed/configuration/JwtConstant.java
+++ user-service/src/main/java/com/syed/configuration/JwtConstant.java
@@ -2,6 +2,6 @@ package com.syed.configuration;
 
 public class JwtConstant {
 
-    public static final String SECRET_KEY = "kjdjdoihskhdjo878789798798ujkjoasou78978978809udjdljdjdjod87879879879879kdsjkjddkjjhkj89787";
+    public static final String SECRET_KEY = "create your secret key";
     public static final String JWT_HEADER="Authorization";
 }
